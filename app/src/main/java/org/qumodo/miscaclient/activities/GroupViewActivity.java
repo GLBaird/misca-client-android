c6709d44a69bb8ef1b7967cf538013226179f528
Leon Baird <leonbaird@mac.com>
2017-11-14 23:46:31 +0000

    User icon in actionbar

--- app/src/main/java/org/qumodo/miscaclient/activities/GroupViewActivity.java
+++ app/src/main/java/org/qumodo/miscaclient/activities/GroupViewActivity.java
@@ -1,20 +1,24 @@
 package org.qumodo.miscaclient.activities;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -26,6 +30,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.qumodo.data.DataManager;
 import org.qumodo.data.MediaLoader;
+import org.qumodo.data.MediaLoaderListener;
 import org.qumodo.data.models.Message;
 import org.qumodo.miscaclient.BuildConfig;
 import org.qumodo.miscaclient.R;
@@ -35,7 +40,6 @@ import org.qumodo.miscaclient.dataProviders.LocationProvider;
 import org.qumodo.miscaclient.dataProviders.UserSettingsManager;
 import org.qumodo.miscaclient.fragments.MessageListFragment;
 import org.qumodo.miscaclient.fragments.ObjectSearchFragment;
-import org.qumodo.miscaclient.fragments.QMiscaGroupsListFragment;
 import org.qumodo.network.QMessage;
 import org.qumodo.network.QMessageType;
 import org.qumodo.services.QTCPSocketService;
@@ -70,7 +74,6 @@ public class GroupViewActivity extends Activity implements MessageListFragment.O
                     Intent startupActivity = new Intent(GroupViewActivity.this, StartupActivity.class);
                     startupActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(startupActivity);
-
                     finish();
                     break;
                 case ObjectSearchFragment.ACTION_CLASSIFICATION_RESULT:
@@ -94,8 +97,32 @@ public class GroupViewActivity extends Activity implements MessageListFragment.O
         setContentView(R.layout.activity_group_view);
 
         actionBar = getActionBar();
-        if (actionBar != null)
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+            MediaLoader.getUserCircularAvatar(groupID, new MediaLoaderListener() {
+                @Override
+                public void imageHasLoaded(String ref, Bitmap image, double scale) {
+                    DisplayMetrics displaymetrics = getResources().getDisplayMetrics();
+                    float size = TypedValue.applyDimension( TypedValue.COMPLEX_UNIT_DIP, 40, displaymetrics );
+                    image = Bitmap.createScaledBitmap(image, Math.round(size), Math.round(size), false);
+                    Bitmap padded = Bitmap.createBitmap(Math.round((float)((float)image.getWidth() * 1.35)), image.getHeight(), image.getConfig());
+                    Canvas c = new Canvas(padded);
+                    c.drawBitmap(image, 0, 0, null);
+                    BitmapDrawable icon = new BitmapDrawable(getResources(), padded);
+                    actionBar.setIcon(icon);
+                    actionBar.setDisplayUseLogoEnabled(true);
+                    actionBar.setDisplayOptions(
+                        ActionBar.DISPLAY_HOME_AS_UP |
+                        ActionBar.DISPLAY_USE_LOGO |
+                        ActionBar.DISPLAY_SHOW_TITLE |
+                        ActionBar.DISPLAY_SHOW_HOME
+                    );
+                }
+
+                @Override
+                public void imageHasFailedToLoad(String ref) {}
+            });
+        }
 
         MessageListFragment fragment = new MessageListFragment();
         fragment.setGroup(groupID, getApplicationContext());
