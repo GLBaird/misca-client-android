0e18cd5739c3773f86079120966830adde00345a
cclcalder <cclcalder@gmail.com>
2017-11-21 20:44:35 +0000

    Additional messaging data (#1)

--- app/src/main/res/raw/sample_data_messages.sql
+++ app/src/main/res/raw/sample_data_messages.sql
@@ -1,68 +1,256 @@
 INSERT INTO messages VALUES (
-    "QSDM_1122334455", "QSDU_1234567890", "QSDG_1122334455", 1505423031451, 0,
-    '{"text":"Cool, thanks"}', 0, 0
+    "QSDM_1122334411", "QSDU_1234567890", "QSDG_1122334455", 1505423031451, 0,
+    '{"text":"Hey, thanks for connecting with me"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_2748374837", "QSDU_3344554656", "QSDG_1873743783", 1505123031451, 0,
-    '{"text":"Hi there,"}', 0, 0
+    "QSDM_1122334422", "QSDU_1122334455", "QSDG_1122334455", 1505433031451, 0,
+    '{"text":"Hey, no problem"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_2343784734", "QSDU_3344554656", "QSDG_1873743783", 1505071416200, 0,
-    '{"text":"My next message will be an image, that is, when we get images working!"}', 0, 0
+    "QSDM_1122334433", "QSDU_1234567890", "QSDG_1122334455", 1505443031451, 0,
+    '{"text":"Hopefully this tool will work well for both of us, and help with sharing info on the case!"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_2858584998", "QSDU_3344554656", "QSDG_1873743783", 1505081416200, 1,
+    "QSDM_1122334444", "QSDU_1122334455", "QSDG_1122334455", 1505453031451, 0,
+    '{"text":"Yeah, very keen to get going.."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_2343784766", "QSDU_3344554656", "QSDG_1873743783", 1505071416200, 0,
+    '{"text":"Hey mate have you seen this?!"}', 0, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_2858584977", "QSDU_3344554656", "QSDG_1873743783", 1505081416200, 1,
     '{"caption":"Here is my image!"}', 0, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_3745746343", "QSDU_6473645748", "QSDG_2845748584", 1505071416200, 0,
+    "QSDM_2748374855", "QSDU_3344554656", "QSDG_1873743783", 1505123031451, 0,
+    '{"text":"Pretty cool right"}', 0, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_2748374955", "QSDU_1122334455", "QSDG_1873743783", 1505123031452, 0,
+    '{"text":"Will check it out when I have time"}', 0, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_2748374155", "QSDU_1122334455", "QSDG_1873743783", 1505123031453, 0,
+    '{"text":"Have you run Misca on it?"}', 0, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_2748374255", "QSDU_6473645748", "QSDG_1873743783", 1505123031454, 0,
+    '{"text":"Looks interesting to me!"}', 0, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_2748374355", "QSDU_3344554656", "QSDG_1873743783", 1505123031455, 0,
+    '{"text":"Yeah will send over misca info when I have a chance. Or run it yourself. And let me know the results.."}', 0, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_3745746388", "QSDU_6473645748", "QSDG_2845748584", 1505071416200, 0,
     '{"text":"Hello there!"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_3745746347", "QSDU_6473645748", "QSDG_2845748584", 1505081416200, 0,
+    "QSDM_3745746399", "QSDU_6473645748", "QSDG_2845748584", 1505081416200, 0,
     '{"text":"Can you review the data sent?"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_4585847474", "QSDU_0000000000", "QSDG_3454958494", 1505071416200, 0,
+    "QSDM_3745746499", "QSDU_6473645748", "QSDG_2845748584", 1505081416210, 0,
+    '{"text":"Waiting on feedback so would be good to hear what you have to say..."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_3745746599", "QSDU_1122334455", "QSDG_2845748584", 1505081416220, 0,
+    '{"text":"No problem will report back this afternoon, run Misca on the images if you get a chance too."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_4585847410", "QSDU_1122334455", "QSDG_3454958494", 1505071416200, 0,
     '{"text":"I have found a match: BD51 SMR"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_5454545415", "QSDU_1928387475", "QSDG_4598958659", 1505071416200, 0,
+    "QSDM_5454545412", "QSDU_1928387475", "QSDG_4598958659", 1505071416200, 0,
     '{"text":"Hi there"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_5454545417", "QSDU_1928387475", "QSDG_4598958659", 1505091416200, 0,
-    '{"text":"Can you check this image?"}', 1, 0
+    "QSDM_5454545413", "QSDU_1928387475", "QSDG_4598958659", 1505091416200, 0,
+    '{"text":"Can you check the images in the District 9 Group??"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6565434435", "QSDU_3344554656", "QSDG_5984958323", 1503041416200, 0,
-    '{"text":"That is what I thought too!"}', 1, 0
+    "QSDM_5454545423", "QSDU_1928387475", "QSDG_4598958659", 1505091416300, 0,
+    '{"text":"I think a couple of them will be interesting for you"}', 0, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6565434535", "QSDU_1122334455", "QSDG_6858574843", 1505051416200, 0,
+    "QSDM_6565434414", "QSDU_3344554656", "QSDG_5984958323", 1503041416100, 0,
+    '{"text":"Hey. I think the man in that photo is Joe Salmon, he is a suspect in the case and that image is another piece of evidence connecting him to the gang and Peter Greenwich. Let me know if you have any more info surrounding this."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565434425", "QSDU_1122334455", "QSDG_5984958323", 1503041416200, 0,
+    '{"text":"That is what I thought too! I will pass info on to my boss and let you know.."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565434515", "QSDU_1122334455", "QSDG_6858574843", 1505051416200, 0,
     '{"text":"Hey guys"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6515434435", "QSDU_3344554656", "QSDG_6858574843", 1505066416200, 0,
+    "QSDM_6565434416", "QSDU_3344554656", "QSDG_6858574843", 1505055416200, 0,
+    '{"text":"Hey!"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6515434417", "QSDU_3344554656", "QSDG_6858574843", 1505066416200, 0,
     '{"text":"What you up to?"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6565424435", "QSDU_1928387475", "QSDG_6858574843", 1505068416200, 0,
+    "QSDM_6565424418", "QSDU_1928387475", "QSDG_6858574843", 1505068416200, 0,
     '{"text":"Was that you Tom the other day?"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6565434635", "QSDU_4458748473", "QSDG_6858574843", 1505069416200, 0,
+    "QSDM_6565434319", "QSDU_1122334455", "QSDG_6858574843", 1505069416200, 0,
     '{"text":"Yes it was!!"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6575434435", "QSDU_9874837298", "QSDG_5845485744", 1505066416200, 0,
+    "QSDM_6565434320", "QSDU_1122334455", "QSDG_6858574843", 1505069596200, 0,
+    '{"text":"Where were you guys??"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565424921", "QSDU_1928387475", "QSDG_6858574843", 1505070096200, 0,
+    '{"text":"We were near the bridge"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565424922", "QSDU_1928387475", "QSDG_6858574843", 1505071096200, 0,
+    '{"text":"Nothing happened in the end"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565424923", "QSDU_1928387475", "QSDG_6858574843", 1505071296200, 0,
+    '{"text":"Interesting though, watching suspect from last weeks attack"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565434324", "QSDU_1122334455", "QSDG_6858574843", 1505072296200, 0,
+    '{"text":"Whats the latest with that? - not caught up with Andy"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565424925", "QSDU_1928387475", "QSDG_6858574843", 1505073296200, 0,
+    '{"text":"Will send over some photos we took"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565436126", "QSDU_1122334455", "QSDG_6858574843", 1505272296200, 0,
+    '{"text":"Hey will you send over those photos you mentioned"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6511124927", "QSDU_1928387475", "QSDG_6858574843", 1505275296200, 0,
+    '{"text":"Oops! Sending now.."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6511124998", "QSDU_1928387475", "QSDG_6858574843", 1505285296200, 1,
+    '{"caption":"Image1"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6511124999", "QSDU_1928387475", "QSDG_6858574843", 1505298296200, 1,
+    '{"caption":"Image2"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565436130", "QSDU_1122334455", "QSDG_6858574843", 1505308296200, 0,
+    '{"text":"Cheers"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6565657631", "QSDU_1122334455", "QSDG_5845485744", 1505061616200, 0,
+    '{"text":"Did you send that information?"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6156557632", "QSDU_9874837298", "QSDG_5845485744", 1505061816200, 0,
+    '{"text":"Yeah check the other group, its in there."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434433", "QSDU_9874837298", "QSDG_5845485744", 1505066416200, 0,
     '{"text":"Hey, when you are away we will look into it"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6565657676", "QSDU_1122334455", "QSDG_5845485744", 1505061616200, 0,
-    '{"text":"Can you send some information"}', 1, 0
+    "QSDM_6565657634", "QSDU_1122334455", "QSDG_5845485744", 1505066816200, 0,
+    '{"text":"Thanks that would be great"}', 1, 0
 );
 INSERT INTO messages VALUES (
-    "QSDM_6156557677", "QSDU_9874837298", "QSDG_5845485744", 1505061816200, 0,
-    '{"text":"No problem, we will see how we go!"}', 1, 0
+    "QSDM_6575434435", "QSDU_9874837298", "QSDG_5845485744", 1505278816200, 0,
+    '{"text":"Hey, when do you get back?"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434736", "QSDU_9874837298", "QSDG_5845485744", 1505278816200, 0,
+    '{"text":"Got some leads on the case want to chat about"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434137", "QSDU_1122334455", "QSDG_5845485744", 1505318816200, 0,
+    '{"text":"Free to chat now"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434138", "QSDU_9874837298", "QSDG_5845485744", 1505278816200, 0,
+    '{"text":"Have you seen this image? Might be relevant to your work atm"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434133", "QSDU_9874837298", "QSDG_5845485744", 1505278816200, 1,
+    '{"caption":"Here is the image"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434140", "QSDU_1122334455", "QSDG_5845485744", 1505319816200, 0,
+    '{"text":"Cheers"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434141", "QSDU_1122334455", "QSDG_5845485744", 1505319896200, 0,
+    '{"text":"I havent seen that before.."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6574434142", "QSDU_9874837298", "QSDG_5845485744", 1505329896200, 0,
+    '{"text":"Chat to Chris, he mentioned it to me, he might have more info you might be interested in"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6573434143", "QSDU_1122334455", "QSDG_5845485744", 1505339896200, 0,
+    '{"text":"Thanks, really helpful. Will do"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6572434144", "QSDU_1122334455", "QSDG_5845485744", 1505349896200, 0,
+    '{"text":"Hope all going well over with you guys!"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434445", "QSDU_9874837298", "QSDG_5845485744", 1505449896200, 0,
+    '{"text":"Hey, take a look at this image"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434456", "QSDU_9874837298", "QSDG_5845485744", 1505499896200, 1,
+    '{"caption":"Here is the image"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434457", "QSDU_3344554656", "QSDG_5845485746", 1505308296000, 0,
+    '{"text":"Hey lets message on here about the plan for next month"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434458", "QSDU_1122334455", "QSDG_5845485746", 1505308296005, 0,
+    '{"text":"Sounds good"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434459", "QSDU_3344554656", "QSDG_5845485746", 1505308296095, 0,
+    '{"text":"Great, will be in touch when the rest of the team gets back to us! How are you doing anyway? Long time no speak"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434460", "QSDU_1122334455", "QSDG_5845485746", 1505308296100, 0,
+    '{"text":"Yeah, really good, very busy at work, lots on our plate, but its going well. Will be good to catch up with you and your team. Hows it going with you?"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434461", "QSDU_3344554656", "QSDG_5845485747", 1504499896200, 0,
+    '{"text":"Hey team!"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434462", "QSDU_1122334455", "QSDG_5845485747", 1504499996200, 0,
+    '{"text":"Hey!"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434463", "QSDU_3344554656", "QSDG_5845485747", 1504500096200, 0,
+    '{"text":"How you all doing?"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434464", "QSDU_1122334455", "QSDG_5845485747", 1504501096200, 0,
+    '{"text":"Did I see you last week Frank? I was in Bristol.."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434465", "QSDU_1122334455", "QSDG_5845485748", 1506501096200, 0,
+    '{"text":"Hi, lets get going.."}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434466", "QSDU_1122334455", "QSDG_5845485749", 1503501096200, 0,
+    '{"text":"Hi, how are you?"}', 1, 0
+);
+INSERT INTO messages VALUES (
+    "QSDM_6575434467", "QSDU_3344554656", "QSDG_5845485749", 1503301096200, 0,
+    '{"text":"Hi there, good how are things? Just started using this, seems good!"}', 1, 0
 );
